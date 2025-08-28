a4aa00ba0aec29ddf3d88ac0f4c2b63f474c0c51
HowardPeter <phuocnt1611@gmail.com>
2025-08-29 02:34:53 +0700

    chore(class sv): add initial files

---
+++ class-service/server.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import morgan from 'morgan';
+import cookieParser from "cookie-parser";
+
+import router from './routes/studentRoutes.js'
+import { errorHandler } from './middleware/error-handler.js'
+import authentication from './middleware/authentication.js'
+
+const app = express();
+const PORT = 3003;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan('dev'));
+
+app.use(authentication);
+
+app.use('/student', router);
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
