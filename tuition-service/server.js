f42fe497eaf7d379495cf1bff03dd11e22f2cb0d
HowardPeter <phuocnt1611@gmail.com>
2025-09-04 03:40:16 +0700

    chore(tuition sv): add tuition service config files

---
+++ tuition-service/server.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import morgan from 'morgan';
+import cookieParser from "cookie-parser";
+
+import router from './routes/classRoutes.js'
+import { authentication } from './middlewares/index.js'
+import { errorHandler } from '#shared/middlewares/index.js';
+
+const app = express();
+const PORT = 3003;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan('dev'));
+
+app.use(authentication);
+
+app.use('/classes', router);
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
