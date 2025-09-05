dfed2657209c207a73282fa58697c570ead2fb1a
HowardPeter <phuocnt1611@gmail.com>
2025-09-06 00:31:54 +0700

    chore(tuition sv): add authentication middleware

--- tuition-service/server.js
+++ tuition-service/server.js
@@ -2,9 +2,8 @@ import express from 'express'
 import morgan from 'morgan';
 import cookieParser from "cookie-parser";
 
-// import router from './routes/classRoutes.js'
-// import { authentication } from './middlewares/index.js'
-// import { errorHandler } from '#shared/middlewares/index.js';
+import { authentication } from './middlewares/index.js'
+import { errorHandler } from '#shared/middlewares/index.js';
 
 const app = express();
 const PORT = 3004;
@@ -13,11 +12,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(morgan('dev'));
 
-// app.use(authentication);
+app.use(authentication);
 
-// app.use('/classes', router);
-
-// app.use(errorHandler);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
