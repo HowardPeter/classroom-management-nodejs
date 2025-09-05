d7d8ec1fe49c13ebbda79b345e5528f39ed9b4c1
HowardPeter <phuocnt1611@gmail.com>
2025-09-06 00:29:56 +0700

    build(tuition sv): create db

--- tuition-service/server.js
+++ tuition-service/server.js
@@ -2,22 +2,22 @@ import express from 'express'
 import morgan from 'morgan';
 import cookieParser from "cookie-parser";
 
-import router from './routes/classRoutes.js'
-import { authentication } from './middlewares/index.js'
-import { errorHandler } from '#shared/middlewares/index.js';
+// import router from './routes/classRoutes.js'
+// import { authentication } from './middlewares/index.js'
+// import { errorHandler } from '#shared/middlewares/index.js';
 
 const app = express();
-const PORT = 3003;
+const PORT = 3004;
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan('dev'));
 
-app.use(authentication);
+// app.use(authentication);
 
-app.use('/classes', router);
+// app.use('/classes', router);
 
-app.use(errorHandler);
+// app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
