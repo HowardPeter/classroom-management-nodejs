bb29d5350e9579d79e947280fbc18d433aabcef9
HowardPeter <phuocnt1611@gmail.com>
2025-09-11 19:32:10 +0700

    build(class sv): add check teacher exist

---
+++ class-service/api/teacherServiceApi.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { InternalServerError } from "#shared/errors/errors.js";
+
+const URL = "http://teacher:3005/teachers"
+
+class TeacherServiceClient {
+  constructor(baseURL) {
+    this.api = axios.create({ baseURL });
+  }
+
+  async getTeacherById(id, accessToken) {
+    try {
+      const res = await this.api.get(`/${id}`, {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      });
+      return res.data;
+    } catch (err) {
+      throw new InternalServerError(`Teacher API returned ${err.response.status}: ${err.response.data?.msg || err.message}`);
+    }
+  }
+}
+
+export default new TeacherServiceClient(URL);
