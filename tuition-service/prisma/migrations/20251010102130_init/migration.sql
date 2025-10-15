087aea2788806e2d84780ca00c4704c9e7a12e61
HowardPeter <phuocnt1611@gmail.com>
2025-10-15 17:03:25 +0700

    conf(database): change prisma schema on tables

--- tuition-service/prisma/migrations/20251010102130_init/migration.sql
+++ tuition-service/prisma/migrations/20251010102130_init/migration.sql
@@ -1,18 +1,21 @@
--- CreateEnum
-CREATE TYPE "public"."InvoiceStatus" AS ENUM ('PENDING', 'PAID', 'OVERDUE', 'PARTIAL', 'CANCELLED');
+-- CreateSchema
+CREATE SCHEMA IF NOT EXISTS "tuition";
 
 -- CreateEnum
-CREATE TYPE "public"."PaymentMethod" AS ENUM ('CASH', 'BANK', 'MOMO', 'OTHER');
+CREATE TYPE "tuition"."InvoiceStatus" AS ENUM ('PENDING', 'PAID', 'OVERDUE', 'PARTIAL', 'CANCELLED');
+
+-- CreateEnum
+CREATE TYPE "tuition"."PaymentMethod" AS ENUM ('CASH', 'BANK', 'MOMO', 'OTHER');
 
 -- CreateTable
-CREATE TABLE "public"."Invoice" (
+CREATE TABLE "tuition"."Invoice" (
     "invoice_id" TEXT NOT NULL,
     "student_id" TEXT NOT NULL,
     "class_id" TEXT NOT NULL,
     "description" TEXT,
     "required_amount" DECIMAL(65,30) NOT NULL,
     "due_date" TIMESTAMP(3),
-    "status" "public"."InvoiceStatus" DEFAULT 'PENDING',
+    "status" "tuition"."InvoiceStatus" DEFAULT 'PENDING',
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
@@ -20,15 +23,15 @@ CREATE TABLE "public"."Invoice" (
 );
 
 -- CreateTable
-CREATE TABLE "public"."Payment" (
+CREATE TABLE "tuition"."Payment" (
     "payment_id" TEXT NOT NULL,
     "invoice_id" TEXT NOT NULL,
     "amount" DECIMAL(65,30) NOT NULL,
-    "method" "public"."PaymentMethod",
+    "method" "tuition"."PaymentMethod",
     "paid_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Payment_pkey" PRIMARY KEY ("payment_id")
 );
 
 -- AddForeignKey
-ALTER TABLE "public"."Payment" ADD CONSTRAINT "Payment_invoice_id_fkey" FOREIGN KEY ("invoice_id") REFERENCES "public"."Invoice"("invoice_id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "tuition"."Payment" ADD CONSTRAINT "Payment_invoice_id_fkey" FOREIGN KEY ("invoice_id") REFERENCES "tuition"."Invoice"("invoice_id") ON DELETE CASCADE ON UPDATE CASCADE;
